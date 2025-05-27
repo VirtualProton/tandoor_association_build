6889357a2ebef6fe4ae87a320dc0afa37d817745
Pritham <impritham@gmail.com>
2025-05-27 07:46:40 +0530

    lease query

--- dist/controllers/LeaseQuery/updateLeaseQuery.controller.js
+++ dist/controllers/LeaseQuery/updateLeaseQuery.controller.js
@@ -17,44 +17,61 @@ const root_1 = require("../../exceptions/root");
 const updateLeaseQueryController = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const updateLeaseQuery = updateLeaseQuery_1.updateLeaseQuerySchema.parse(req.body);
-        const allowedRoles = ["TSMWA_EDITOR", "TQMA_EDITOR", "ADMIN"];
-        if (!allowedRoles.includes(req.user.role)) {
+        if (!["TSMWA_EDITOR", "TQMA_EDITOR", "ADMIN"].includes(req.user.role)) {
             return next(new bad_request_1.BadRequestsException("Unauthorized", root_1.ErrorCode.UNAUTHORIZED));
         }
-        yield __1.prismaClient.$transaction((prisma) => __awaiter(void 0, void 0, void 0, function* () {
-            const { leaseQueryId, deleteAttachment = [], updateAttachments = [], newAttachments = [], membershipId, presentLeaseHolder, dateOfLease, expiryOfLease, dateOfRenewal, status, } = updateLeaseQuery;
-            // Delete attachments
-            if (deleteAttachment.length > 0) {
-                const ids = deleteAttachment.map((a) => a.id);
+        const updatedLeaseQuery = yield __1.prismaClient.$transaction((prisma) => __awaiter(void 0, void 0, void 0, function* () {
+            if (updateLeaseQuery.deleteAttachment &&
+                updateLeaseQuery.deleteAttachment.length > 0) {
                 yield prisma.leaseQueryAttachments.deleteMany({
-                    where: { id: { in: ids } },
+                    where: {
+                        id: {
+                            in: updateLeaseQuery.deleteAttachment.map((attachment) => attachment.id),
+                        },
+                    },
                 });
             }
-            // Update attachments
-            if (updateAttachments.length > 0) {
-                yield Promise.all(updateAttachments.map(({ id, documentName, documentPath }) => prisma.leaseQueryAttachments.update({
-                    where: { id },
-                    data: Object.assign(Object.assign({}, (documentName != null && { documentName })), (documentPath != null && { documentPath })),
+            if (updateLeaseQuery.updateAttachments &&
+                updateLeaseQuery.updateAttachments.length > 0) {
+                yield Promise.all(updateLeaseQuery.updateAttachments.map((attachment) => __awaiter(void 0, void 0, void 0, function* () {
+                    return prisma.leaseQueryAttachments.update({
+                        where: { id: attachment.id },
+                        data: Object.assign(Object.assign({}, (attachment.documentName != null && {
+                            documentName: attachment.documentName,
+                        })), (attachment.documentPath != null && {
+                            documentPath: attachment.documentPath,
+                        })),
+                    });
                 })));
             }
-            // Create new attachments
-            const validNewAttachments = newAttachments.filter((a) => typeof a.documentName === "string" && typeof a.documentPath === "string");
-            if (validNewAttachments.length > 0) {
+            if (updateLeaseQuery.newAttachments &&
+                updateLeaseQuery.newAttachments.length > 0) {
                 yield prisma.leaseQueryAttachments.createMany({
-                    data: validNewAttachments.map(({ documentName, documentPath }) => ({
-                        leaseQueryId,
-                        documentName: documentName,
-                        documentPath: documentPath,
+                    data: updateLeaseQuery.newAttachments
+                        .filter((attachment) => typeof attachment.documentName === "string" &&
+                        typeof attachment.documentPath === "string")
+                        .map((attachment) => ({
+                        documentName: attachment.documentName,
+                        documentPath: attachment.documentPath,
+                        leaseQueryId: updateLeaseQuery.leaseQueryId,
                     })),
                 });
             }
-            // Update lease query details
-            const updateData = Object.assign(Object.assign(Object.assign(Object.assign(Object.assign({}, (typeof presentLeaseHolder === "string" && { presentLeaseHolder })), (dateOfLease != null && { dateOfLease })), (expiryOfLease != null && { expiryOfLease })), (dateOfRenewal != null && { dateOfRenewal })), (status != null && { status }));
-            if (typeof membershipId === "string") {
-                updateData.membership = { connect: { id: membershipId } };
-            }
+            const updateData = Object.assign(Object.assign(Object.assign(Object.assign(Object.assign(Object.assign({}, (typeof updateLeaseQuery.membershipId === "string" && {
+                membershipId: updateLeaseQuery.membershipId,
+            })), (typeof updateLeaseQuery.presentLeaseHolder === "string" && {
+                presentLeaseHolder: updateLeaseQuery.presentLeaseHolder,
+            })), (updateLeaseQuery.dateOfLease != null && {
+                dateOfLease: updateLeaseQuery.dateOfLease,
+            })), (updateLeaseQuery.expiryOfLease != null && {
+                expiryOfLease: updateLeaseQuery.expiryOfLease,
+            })), (updateLeaseQuery.dateOfRenewal != null && {
+                dateOfRenewal: updateLeaseQuery.dateOfRenewal,
+            })), (updateLeaseQuery.status != null && {
+                status: updateLeaseQuery.status,
+            }));
             yield prisma.leaseQuery.update({
-                where: { leaseQueryId },
+                where: { leaseQueryId: updateLeaseQuery.leaseQueryId },
                 data: updateData,
             });
         }));
@@ -63,7 +80,7 @@ const updateLeaseQueryController = (req, res, next) => __awaiter(void 0, void 0,
         });
     }
     catch (error) {
-        next(new bad_request_1.BadRequestsException(`Failed to update lease query ${error.message}`, root_1.ErrorCode.BAD_REQUEST));
+        next(error);
     }
 });
 exports.updateLeaseQueryController = updateLeaseQueryController;
