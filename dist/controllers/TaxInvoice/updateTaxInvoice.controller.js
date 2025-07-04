eff824820bc09c7c49f84ca25a9048ee0c34d7e9
Pritham <impritham@gmail.com>
2025-07-05 02:13:03 +0530

    updateTaxInvoice fixed

--- dist/controllers/TaxInvoice/updateTaxInvoice.controller.js
+++ dist/controllers/TaxInvoice/updateTaxInvoice.controller.js
@@ -27,15 +27,42 @@ const root_1 = require("../../exceptions/root");
 const updateTaxInvoice_1 = require("../../schema/taxInvoice/updateTaxInvoice");
 const updateTaxInvoice = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const taxInvoiceData = updateTaxInvoice_1.TaxInvoicePartialUpdateSchema.parse(req.body);
+        const _a = updateTaxInvoice_1.TaxInvoicePartialUpdateSchema.parse(req.body), { invoiceId, newInvoiceItemSchema, updateInvoiceItemSchema, deleteInvoiceItemSchema } = _a, updateData = __rest(_a, ["invoiceId", "newInvoiceItemSchema", "updateInvoiceItemSchema", "deleteInvoiceItemSchema"]);
         if (!["TSMWA_EDITOR", "TQMA_EDITOR", "ADMIN"].includes(req.user.role)) {
             return next(new bad_request_1.BadRequestsException("Unauthorized", root_1.ErrorCode.UNAUTHORIZED));
         }
-        // Exclude invoiceId from the update data
-        const { invoiceId } = taxInvoiceData, updateData = __rest(taxInvoiceData, ["invoiceId"]);
+        if (deleteInvoiceItemSchema && deleteInvoiceItemSchema.length > 0) {
+            const deleteItems = yield __1.prismaClient.invoiceItem.deleteMany({
+                where: {
+                    id: {
+                        in: deleteInvoiceItemSchema.map(item => item.id),
+                    },
+                    taxInvoice: {
+                        invoiceId: invoiceId,
+                    },
+                },
+            });
+        }
+        if (updateInvoiceItemSchema && updateInvoiceItemSchema.length > 0) {
+            const updateItems = yield Promise.all(updateInvoiceItemSchema.map((item) => __awaiter(void 0, void 0, void 0, function* () {
+                const { id } = item, updateItemData = __rest(item, ["id"]);
+                return yield __1.prismaClient.invoiceItem.update({
+                    where: { id },
+                    data: Object.assign({}, updateItemData),
+                });
+            })));
+        }
+        if (newInvoiceItemSchema && newInvoiceItemSchema.length > 0) {
+            const newItems = yield __1.prismaClient.invoiceItem.createMany({
+                data: newInvoiceItemSchema.map(item => (Object.assign(Object.assign({}, item), { invoiceId: invoiceId }))),
+            });
+        }
         const updatedTaxInvoice = yield __1.prismaClient.taxInvoice.update({
-            where: { invoiceId },
-            data: Object.assign(Object.assign({}, updateData), { modifiedBy: req.user.userId }),
+            where: { invoiceId: invoiceId },
+            data: Object.assign(Object.assign({}, updateData), { modifiedBy: req.user.userId, modifiedAt: new Date() }),
+            include: {
+                invoiceItems: true, // Include updated invoice items
+            },
         });
         return res.status(200).json({
             message: "Tax invoice updated successfully",
--- dist/schema/taxInvoice/updateTaxInvoice.js
+++ dist/schema/taxInvoice/updateTaxInvoice.js
@@ -1,22 +1,19 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TaxInvoicePartialUpdateSchema = void 0;
 const zod_1 = require("zod");
-// Base fields excluding invoiceId
-const TaxInvoiceFieldsOptional = zod_1.z.object({
-    membershipId: zod_1.z.string().max(225).optional(),
-    invoiceDate: zod_1.z.coerce.date().optional(),
+// Schema for each invoice item
+const newInvoiceItemSchema = zod_1.z.object({
+    hsnCode: zod_1.z.string().max(225),
+    particular: zod_1.z.string(),
+    stoneCount: zod_1.z.number().int(),
+    size: zod_1.z.coerce.number().nonnegative().max(99999999.99), // Decimal(10,2)
+    totalSqFeet: zod_1.z.coerce.number().nonnegative().max(99999999.99),
+    ratePerSqFeet: zod_1.z.coerce.number().nonnegative().max(99999999.99),
+    amount: zod_1.z.coerce.number().nonnegative().max(99999999.99),
+});
+const updateInvoiceItemSchema = zod_1.z.object({
+    id: zod_1.z.number().int(),
     hsnCode: zod_1.z.string().max(225).optional(),
     particular: zod_1.z.string().optional(),
     stoneCount: zod_1.z.number().int().optional(),
@@ -24,18 +21,27 @@ const TaxInvoiceFieldsOptional = zod_1.z.object({
     totalSqFeet: zod_1.z.coerce.number().nonnegative().max(99999999.99).optional(),
     ratePerSqFeet: zod_1.z.coerce.number().nonnegative().max(99999999.99).optional(),
     amount: zod_1.z.coerce.number().nonnegative().max(99999999.99).optional(),
+}).refine((data) => Object.keys(Object.assign(Object.assign({}, data), { id: undefined })).some((key) => key !== "id" && data[key] !== undefined), {
+    message: "At least one field other than id must be provided",
+    path: [],
+});
+const deleteInvoiceItemSchema = zod_1.z.object({
+    id: zod_1.z.number().int(),
+});
+// Full schema for TaxInvoice with nested invoice items
+exports.TaxInvoicePartialUpdateSchema = zod_1.z.object({
+    invoiceId: zod_1.z.string().max(225),
+    membershipId: zod_1.z.string().max(225).optional(),
+    invoiceDate: zod_1.z.coerce.date().optional(),
     cGSTInPercent: zod_1.z.number().int().optional(),
     sGSTInPercent: zod_1.z.number().int().optional(),
     iGSTInPercent: zod_1.z.number().int().optional(),
     subTotal: zod_1.z.coerce.number().nonnegative().max(99999999.99).optional(),
     total: zod_1.z.coerce.number().nonnegative().max(99999999.99).optional(),
-});
-// Full schema with invoiceId required
-exports.TaxInvoicePartialUpdateSchema = TaxInvoiceFieldsOptional.extend({
-    invoiceId: zod_1.z.string().max(225),
-}).refine((data) => {
-    const { invoiceId } = data, rest = __rest(data, ["invoiceId"]);
-    return Object.values(rest).some((value) => value !== undefined);
-}, {
+    newInvoiceItemSchema: zod_1.z.array(newInvoiceItemSchema).optional(),
+    updateInvoiceItemSchema: zod_1.z.array(updateInvoiceItemSchema).optional(),
+    deleteInvoiceItemSchema: zod_1.z.array(deleteInvoiceItemSchema).optional(),
+}).refine((data) => Object.keys(Object.assign(Object.assign({}, data), { invoiceId: undefined })).some((key) => key !== "invoiceId" && data[key] !== undefined), {
     message: "At least one field other than invoiceId must be provided",
+    path: [],
 });
